253cecbb703b6a9828fae39f5061c75b352169e2
tkosht <takehito.oshita.business@gmail.com>
2025-06-15 22:08:56 +0900

    mod: updated cognee mcp method

--- bin/add_cognee_mcp.sh
+++ bin/add_cognee_mcp.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/sh
 
-# claude mcp add cognee --transport sse http://localhost:8000/sse
+claude mcp add cognee --transport sse http://localhost:8000/sse
 
-repo_dir="$HOME/workspace/dev-tools/external-repos"
-. $repo_dir/.env
-
-claude mcp add cognee \
-  -- uv --directory $repo_dir/cognee/cognee-mcp run cognee
-
-#   -s project \
-#   -e ENV=local \
-#   -e TOKENIZERS_PARALLELISM=false \
-#   -e LLM_API_KEY="$LLM_API_KEY" \
-#   -e EMBEDDING_PROVIDER=$EMBEDDING_PROVIDER \
-#   -e EMBEDDING_MODEL=$EMBEDDING_MODEL \
-#   -e EMBEDDING_DIMENSIONS=$EMBEDDING_DIMENSIONS \
-#   -e EMBEDDING_MAX_TOKENS=$EMBEDDING_MAX_TOKENS \
+# repo_dir="$HOME/workspace/dev-tools/external-repos"
+# . $repo_dir/.env
+# 
+# claude mcp add cognee \
+#   -- uv --directory $repo_dir/cognee/cognee-mcp run cognee
 
